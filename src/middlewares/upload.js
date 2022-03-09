e181837913e8e76e07637b83409c69bee1aecc9f
muthamizhselvan R <53464146+muthamizhyadav@users.noreply.github.com>
2022-03-09 18:09:35 +0530

    upload working

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -15,7 +15,7 @@ const upload = multer({
     storage: storage,
     fileFilter: function(req, file, callback){
         if(
-            file.mimetype == "image/png" || file.mimetype == "image/jpg"
+            file.mimetype === "image/png" || file.mimetype === "image/jpg"
         ){
             callback(null, true)
         }else{
