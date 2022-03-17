b699711a3e1fab421093ad39037c18f024d726a0
muthamizhselvan R <53464146+muthamizhyadav@users.noreply.github.com>
2022-03-17 11:58:45 +0530

    changed

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -3,48 +3,42 @@ const { Category } = require('../models');
 const ApiError = require('../utils/ApiError');
 
 const createcategory = async (categoryBody) => {
-    return Category.create(categoryBody);
-  };
-  const getcategoryById = async (id) => {
-    const category=Category.findOne({active:true});
-    if(!category){
-    throw new ApiError(httpStatus.NOT_FOUND,"Category Not Found")
-    }
-    return category
+  return Category.create(categoryBody);
+};
+const getcategoryById = async (id) => {
+  const category = Category.findOne({ active: true });
+  if (!category) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Category Not Found');
+  }
+  return category;
+};
 
+const querycategory = async (filter, options) => {
+  return Category.paginate(filter, options);
+};
 
-  };
+const updatecategoryById = async (categoryId, updateBody) => {
+  let cate = await getcategoryById(categoryId);
+  if (!cate) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+  }
+  cate = await category.findByIdAndUpdate({ _id: categoryId }, updateBody, { new: true });
+  return cate;
+};
 
-  const querycategory = async (filter, options) => {
-    return Category.paginate(filter, options);
-  };
+const deletecategoryById = async (categoryId) => {
+  const category = await getcategoryById(categoryId);
+  if (!category) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
+  }
+  (category.active = false), (category.archive = true), await category.save();
+  return category;
+};
 
-  const updatecategoryById = async (categoryId, updateBody) => {
-    let cate = await getcategoryById(categoryId);
-    if (!cate) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
-    }
-    cate = await category.findByIdAndUpdate({ _id: categoryId }, updateBody, { new: true });
-    return cate;
-  };
-
-  const deletecategoryById = async (categoryId) => {
-    const category = await getcategoryById(categoryId);
-    if (!category) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'Product not found');
-    }
-   category.active = false,
-   category.archive = true,
-   await category.save() 
-    return category;
-  };
-
-  module.exports = {
-      createcategory,
-      getcategoryById,
-      updatecategoryById,
-      deletecategoryById,
-      querycategory,
-
-  };
-  
+module.exports = {
+  createcategory,
+  getcategoryById,
+  updatecategoryById,
+  deletecategoryById,
+  querycategory,
+};
