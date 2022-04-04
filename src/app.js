e0805a8224b5c084e7674ce6cca502175b2c4757
Muthamizh <uyardev@gmail.com>
2022-04-04 17:57:28 +0530

    gg

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ const { errorConverter, errorHandler } = require('./middlewares/error');
 const ApiError = require('./utils/ApiError');
 const cookieparser = require('cookie-parser')
 const app = express();
+const path = require('path')
 
 if (config.env !== 'test') {
   app.use(morgan.successHandler);
@@ -63,8 +64,13 @@ app.use(errorConverter);
 
 // handle error
 app.use(errorHandler);
-app.use(
-  '/C:UsersSYSTEM 10Downloads\node-express-boilerplate-masterUyarchisrcmiddlewares.uploads',
-  express.static('uploads')
-);
+app.use(express.static(path.join(__dirname, "./src/public")));
+// app.use(express.static(__dirname+'./public')); 
+app.use('/images', express.static('images'));
+
+// app.use(express.static("public"));
+// app.use(
+//   '/C:UsersSYSTEM 10Downloads\node-express-boilerplate-masterUyarchisrcmiddlewares.uploads',
+//   express.static('uploads')
+// );
 module.exports = app;
--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -2,9 +2,9 @@ const multer = require('multer');
 const path = require('path');
 
 const storage = multer.diskStorage({
-  // destination: function(req,res, cb){
-  //    // cb(null, path.join(__dirname, '/uploads/'));
-  // },
+  destination: function(req,res, cb){
+     cb(null, path.join(__dirname, '../public/images'));
+  },
   filename: function (req, file, cb) {
     let ext = path.extname(file.originalname);
     cb(null, Date.now() + ext);
