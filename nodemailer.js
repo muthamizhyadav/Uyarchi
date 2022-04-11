0c81172b7a5ac034fdc9c194fcf30d7bd6d50e24
muthamizhyadav <muthamizhyadav@gmail.com>
2022-04-11 23:42:03 +0530

    SMTP

---
+++ nodemailer.js
@@ -0,0 +1,47 @@
+const nodeMailer = require('nodemailer')
+
+// function generateOTP() {
+          
+//     // Declare a digits variable 
+//     // which stores all digits
+//     var digits = '0123456789';
+//     let OTP = '';
+//     for (let i = 0; i < 4; i++ ) {
+//         OTP += digits[Math.floor(Math.random() * 10)];
+//     }
+//     return OTP;
+// }
+const generateOTP = (()=>{
+    var digits = '0123456789';
+    let OTP = '';
+    for (let i=0; i<4; i++){
+        OTP+=digits[Math.floor(Math.random()*10)];
+    }
+    return OTP
+})
+
+// }
+
+const transpoter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    auth: {
+        user:"muthamizhyadav@gmail.com",
+        pass:"dramjibzgemvmmsp"
+    }
+});
+
+const options = {
+    from:"muthamizhyadav@gmail.com",
+    to:"muthamizh153@gmail.com",
+    text:`Hai Your OTP Is:${generateOTP()}`,
+    subject:" Wow That is Simple"
+};
+
+transpoter.sendMail(options, (err, info)=>{
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("send"+info.response);
+} )
