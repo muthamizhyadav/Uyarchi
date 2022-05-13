6af5912003b78034d3812157001906a3a41bc900
Muthamizh <uyardev@gmail.com>
2022-05-13 11:56:25 +0530

    klkl

--- src/middlewares/apartmentImage.js
+++ src/middlewares/apartmentImage.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     },
     filename: function (req, file, cb) {
       let ext = path.extname(file.originalname);
-      cb(null, Date.now() + ext);
+      cb(null,  ext);
     },
   });
 
