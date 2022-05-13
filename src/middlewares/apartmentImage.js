09d6ed7852f6787015e21b47b680526a6a181bac
Muthamizh <uyardev@gmail.com>
2022-05-13 13:06:04 +0530

    ll

--- src/middlewares/apartmentImage.js
+++ src/middlewares/apartmentImage.js
@@ -1,34 +1,30 @@
 const multer = require('multer');
 const path = require('path');
-const rand = require('random-key')
-
-
-
 
 const storage = multer.diskStorage({
-    destination: function(req,res, cb){
-       cb(null, path.join(__dirname, '../../public/images/apartment'));
-    },
-    filename: function (req, file, cb) {
-      let ext = path.extname(file.originalname);
-    let filename = file.originalname
-      cb(null, filename);
-    },
-  });
+  destination: function (req, res, cb) {
+    cb(null, path.join(__dirname, '../../public/images/apartment'));
+  },
+  filename: function (req, file, cb) {
+    let ext = path.extname(file.originalname);
+    let filename = file.originalname;
+    cb(null, filename);
+  },
+});
 
 const uploadImage = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-      if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
-        callback(null, true);
-      } else {
-        console.log('Only png And Jpg file supported!');
-        callback(null, false);
-      }
-    },
-    limits: {
-      fileSize: 3024 * 3024 * 3,
-    },
-  });
+  storage: storage,
+  fileFilter: function (req, file, callback) {
+    if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+      callback(null, true);
+    } else {
+      console.log('Only png And Jpg file supported!');
+      callback(null, false);
+    }
+  },
+  limits: {
+    fileSize: 3024 * 3024 * 3,
+  },
+});
 
-  module.exports = uploadImage;
+module.exports = uploadImage;
