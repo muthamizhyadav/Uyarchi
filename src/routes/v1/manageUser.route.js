edbf0adfbcb611d95bf4ce93c2411b4613e43bff
vignesh449143 <101543057+vignesh449143@users.noreply.github.com>
2022-05-16 15:07:05 +0530

    user

--- src/routes/v1/manageUser.route.js
+++ src/routes/v1/manageUser.route.js
@@ -6,7 +6,7 @@ const proof = require('../../middlewares/proof')
 const router = express.Router();
 
 router.route('/').post(uploadImage.fields([{name:"idProofUpload"}, {name:"addressProofUpload"}, {name:"twoWheelerUpload"}]), manageUserController.createmanageUserService);
-// router.route('/').get(manageUserController.getmanageUserServiceAll)
+router.route('/').get(manageUserController.getmanageUserServiceAll)
 router.route('/login').post(manageUserController.login)
 router.route('/:id/:districtId/:zoneId/:wardId/:page').get(manageUserController.getAllManageUserTable)
 router
