04cbcc2fd53708710dd37f126fc44a676f14699e
Muthamizh <uyardev@gmail.com>
2022-05-26 11:04:23 +0530

    lkll

--- src/services/supplier.service.js
+++ src/services/supplier.service.js
@@ -55,9 +55,6 @@ const productDealingWithsupplier = async (id, date) => {
         as: 'products',
       },
     },
-    // {
-    //   $unwind: '$products',
-    // },
     {
       $lookup: {
         from: 'callstatuses',
