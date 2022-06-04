6dbfa520739cb1a0293d34ff622b6e7a1b1c8ccd
Muthamizh <uyardev@gmail.com>
2022-06-04 10:37:33 +0530

    status approved

--- src/services/stsatus.service.js
+++ src/services/stsatus.service.js
@@ -1,6 +1,6 @@
 const httpStatus = require('http-status');
 const { Street } = require('../models');
-const Status = require('../models/status.model')
+const Status = require('../models/status.model');
 const ApiError = require('../utils/ApiError');
 
 const createStatus = async (streetBody) => {
@@ -12,9 +12,35 @@ const getStreetById = async (statusId) => {
   return status;
 };
 
+const getApprovedProductDateWise = async (date) => {
+  return Status.aggregate([
+    {
+      $match: {
+        $and: [{ date: { $eq: date } }],
+      },
+    },
+    {
+      $match: {
+        $and: [{ status: { $eq: 'Approved' } }],
+      },
+    },
+    {
+      $lookup: {
+        from: 'products',
+        localField: 'productid',
+        foreignField: '_id',
+        as: 'productData',
+      },
+    },
+    {
+      $unwind: '$productData',
+    },
+  ]);
+};
+
 const updatestatusById = async (statusId, updateBody) => {
-  let status = await getStreetById(statusId)
-  console.log(status)
+  let status = await getStreetById(statusId);
+  console.log(status);
   if (!status) {
     throw new ApiError(httpStatus.NOT_FOUND, 'status not found');
   }
@@ -34,6 +60,7 @@ const deletestatusById = async (statusId) => {
 module.exports = {
   createStatus,
   getStreetById,
+  getApprovedProductDateWise,
   deletestatusById,
   updatestatusById,
-}
+};
