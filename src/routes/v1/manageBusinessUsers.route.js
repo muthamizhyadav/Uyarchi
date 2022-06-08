be34a90192ea823d8e427477f79713e1fd5428d1
Muthamizh <uyardev@gmail.com>
2022-06-08 10:11:15 +0530

    ll

--- src/routes/v1/manageBusinessUsers.route.js
+++ src/routes/v1/manageBusinessUsers.route.js
@@ -11,15 +11,23 @@ router
     BusinessUsersController.createBusinessUsers
   )
   .get(BusinessUsersController.getAllBusinessUsers);
-router.route('/superAdmin/List').get(BusinessUsersController.gettAllSuperAdminAssign)
-  router.route('/superAdmin').post(BusinessUsersUpload.fields([{ name: 'idproof' }, { name: 'addsproof' }, { name: 'biodata' }]), BusinessUsersController.createSuperAdminwardAssign)
+router.route('/superAdmin/List').get(BusinessUsersController.gettAllSuperAdminAssign);
+router
+  .route('/superAdmin')
+  .post(
+    BusinessUsersUpload.fields([{ name: 'idproof' }, { name: 'addsproof' }, { name: 'biodata' }]),
+    BusinessUsersController.createSuperAdminwardAssign
+  );
 router
   .route('/manageBusinessUsers/:BUId')
-  .put(BusinessUsersUpload.fields([{ name: 'idproof' }, { name: 'addsproof' }, { name: 'biodata' }]), BusinessUsersController.updateBusinessUsers)
+  .put(
+    BusinessUsersUpload.fields([{ name: 'idproof' }, { name: 'addsproof' }, { name: 'biodata' }]),
+    BusinessUsersController.updateBusinessUsers
+  )
   .delete(BusinessUsersController.deleteBusinessUsers)
-  .get(BusinessUsersController.getBusinessUsersById)
-router.route('/sixRoles').get(BusinessUsersController.getSixRoles)
+  .get(BusinessUsersController.getBusinessUsersById);
+router.route('/sixRoles').get(BusinessUsersController.getSixRoles);
 
 router.route('/superAdmin/:id').get(BusinessUsersController.getSuperAdminAssignById);
-router.route('/superAdmin').get(BusinessUsersController.superAdminAggregation)
+router.route('/superAdmin').get(BusinessUsersController.superAdminAggregation);
 module.exports = router;
