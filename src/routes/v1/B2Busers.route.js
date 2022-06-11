2e556a0b1820378fd4fe764ac2ea2ccbd7f8b636
muthamizhyadav <muthamizhyadav@gmail.com>
2022-06-11 12:47:21 +0530

    ll

--- src/routes/v1/B2Busers.route.js
+++ src/routes/v1/B2Busers.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const b2bUsersController = require('../../controllers/B2BUser.Controller');
 const router = express.Router();
+const cookieparser=require("cookie-parser")
 
 router.post('/', b2bUsersController.createB2bUsers);
-router.post('/login', b2bUsersController.B2bUsersLogin);
+router.post('/login',cookieparser("tokens"), b2bUsersController.B2bUsersLogin);
 router.get('/logout', b2bUsersController.B2bUsersLogout);
 module.exports = router;
