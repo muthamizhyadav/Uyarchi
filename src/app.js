4dc21c739f5ebb9c3fc85925b2f550e2aa92981a
muthamizhyadav <muthamizhyadav@gmail.com>
2022-06-11 13:02:25 +0530

    ll

--- src/app.js
+++ src/app.js
@@ -43,11 +43,15 @@ app.use(compression());
 
 // enable cors
 app.use(cors());
-app.all('*', function (req, res) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-})
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 // app.options('*', cors());
 
 // jwt authentication
