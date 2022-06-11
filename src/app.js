bdacc33165f6768e063b6789c80db63dc5290585
muthamizhyadav <muthamizhyadav@gmail.com>
2022-06-11 12:58:17 +0530

    ll

--- src/app.js
+++ src/app.js
@@ -42,11 +42,12 @@ app.use(mongoSanitize());
 app.use(compression());
 
 // enable cors
-const corsconfig ={
-  credentials:true,
-  origin:'*',
-}
-app.use(cors(corsconfig));
+app.use(cors());
+app.all('*', function (req, res) {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+})
 // app.options('*', cors());
 
 // jwt authentication
