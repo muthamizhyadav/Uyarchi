d783eb7a1ce1361470df6e553211e7213d7b463e
muthamizhyadav <muthamizhyadav@gmail.com>
2022-06-11 17:05:43 +0530

    ll

--- src/routes/v1/verify.authentication.route.js
+++ src/routes/v1/verify.authentication.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authorization = require('../../controllers/tokenVerify.controller');
 const MessageController = require('../../controllers/verifyMessage.controller');
-router.route('/').get(MessageController.Message);
+router.route('/').get(authorization, MessageController.Message);
 
 module.exports = router;
