5ea16d8ba68c516b5d64207d16a601c4228f5e23
bharathidev0303 <bharathiraja996574@gmail.com>
2022-06-25 10:17:51 +0530

    df

--- src/routes/v1/B2Busers.route.js
+++ src/routes/v1/B2Busers.route.js
@@ -19,4 +19,7 @@ router
   .delete(b2bUsersController.deleteMetaUser);
 router.route('/changePassword').put(authorization, b2bUsersController.changePassword);
 router.route('/getusers/salesExecute').get(b2bUsersController.getsalesExecuteRolesUsers)
+router.route('/updatemeta/byuser').post(b2bUsersController.updatemetadata)
 module.exports = router;
+
+
