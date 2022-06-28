2a40431d764287ba48d969ec8e1e71aa7bcbdba9
muthamizhyadav <muthamizhyadav@gmail.com>
2022-06-28 15:03:31 +0530

    hh

--- src/routes/v1/b2bBillStatus.Route.js
+++ src/routes/v1/b2bBillStatus.Route.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.route('/').post(b2bbillStatusController.createB2bBillStatus);
 router.route('/:page').get(b2bbillStatusController.getDataForAccountExecutive);
-router.route('/:id').put(b2bbillStatusController.ManageDeliveryExpenseBillEntry).get(b2bbillStatusController.getBillstatusById);
+router.route('/getData/:id').put(b2bbillStatusController.ManageDeliveryExpenseBillEntry).get(b2bbillStatusController.getBillstatusById);
 router.route('/supplierbills/:date/:page').get(b2bbillStatusController.getBilledDataForSupplierBills);
 module.exports = router;
