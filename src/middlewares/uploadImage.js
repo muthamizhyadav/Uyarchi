ca23d25c4a1dc14722522870f26b189f66d67920
muthamizhyadav <muthamizhyadav@gmail.com>
2022-07-06 14:55:28 +0530

    imaghes

--- src/middlewares/uploadImage.js
+++ src/middlewares/uploadImage.js
@@ -2,14 +2,15 @@ const multer = require('multer');
 const path = require('path');
 
 
-
+let counts = 0;
 const storage = multer.diskStorage({
     destination: function(req,res, cb){
        cb(null, path.join(__dirname, '../../public/images/proofs'));
     },
     filename: function (req, file, cb) {
       let ext = path.extname(file.originalname);
-      cb(null, Date.now() + ext);
+      counts++;
+      cb(null, Date.now() + counts.toString() + ext);
     },
   });
 
