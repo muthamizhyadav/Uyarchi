ccb94d4956981d341312dca14c752581d962390e
vignesh449143 <101543057+vignesh449143@users.noreply.github.com>
2022-07-14 14:35:54 +0530

    third api

--- src/middlewares/shopImage.js
+++ src/middlewares/shopImage.js
@@ -5,27 +5,43 @@ const path = require('path');
 
 const storage = multer.diskStorage({
     destination: function(req,res, cb){
-       cb(null, path.join(__dirname, '../../public/images/shop'));
+     cb(null, path.join(__dirname, '../../public/images/shop'));
     },
     filename: function (req, file, cb) {
+      console.log(file,"sdfsdfsdfsdfsdf")
       let ext = path.extname(file.originalname);
       cb(null, Date.now() + ext);
     },
   });
 
-const uploadImage = multer({
-    storage: storage,
-    fileFilter: function (req, file, callback) {
-      if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
-        callback(null, true);
-      } else {
-        console.log('Only png And Jpg file supported!');
-        callback(null, false);
-      }
-    },
-    limits: {
-      fileSize: 3024 * 3024 * 3,
-    },
-  });
+// const uploadImage = multer({
+//     storage: storage,
+//     fileFilter: function (req, file, callback) {
+//       if (file.mimetype == 'image/png' || file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg') {
+//         callback(null, true);
+//       } else {
+//         console.log('Only png And Jpg file supported!');
+//         callback(null, false);
+//       }
+//     },
+//     limits: {
+//       fileSize: 3024 * 3024 *10,
+//     },
+//   });
 
-  module.exports = uploadImage;
+const multerFilter = (req, file, cb) => {
+  // console.log(file,"sdfsdfsdf")
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb("Not an image! Please upload only images.", false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: multerFilter,
+});
+
+
+  module.exports = upload;
