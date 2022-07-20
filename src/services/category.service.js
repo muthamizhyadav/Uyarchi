4e44f426db10231c914ed98e611fb95ae0e276b5
muthamizhyadav <muthamizhyadav@gmail.com>
2022-07-20 14:46:26 +0530

    ggg

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -100,9 +100,9 @@ const deletecategoryById = async (categoryId) => {
 
 const categoryPagination = async (page) => {
   const cat = await Category.aggregate([
-    {
-      $sort: { _id: 1 },
-    },
+    // {
+    //   $sort: { _id: -1 },
+    // },
     { $skip: 10 * page },
     { $limit: 10 },
   ]);
