3333f1853acd3017afebd9d6b29f2ad5837b777d
Vidhya150620 <vidhyavidhu1506@gmail.com>
2022-07-21 11:42:48 +0530

    pagination

--- src/routes/v1/b2b.monthlyRecuring.route.js
+++ src/routes/v1/b2b.monthlyRecuring.route.js
@@ -4,6 +4,7 @@ const router = express.Router();
 // const  monthlyRecuring= require('../../middlewares/');
 
 router.route('/add').post(monthlyRecuringController.createRecuring);
-router.route('/getAll').get(monthlyRecuringController.getRecuring)
+router.route('/getAll').get(monthlyRecuringController.getRecuring);
+router.route('/getpage/:page').get(monthlyRecuringController.getMonthlyRecuring);
 
 module.exports = router;
