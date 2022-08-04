53fbcba8ff54b602715a885c591cc51422e12019
Vidhya150620 <vidhyavidhu1506@gmail.com>
2022-08-04 11:20:03 +0530

    ward

--- src/services/b2b.wardAdmin.service.js
+++ src/services/b2b.wardAdmin.service.js
@@ -2,34 +2,34 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const { ShopOrderClone } = require('../models/shopOrder.model');
 const { ProductorderClone } = require('../models/shopOrder.model');
-const { Shop } = require('../models/b2b.ShopClone.model');
+const { Shop } = require('../models/b2b.ShopClone.model')
+
 
 // GET DETAILS
 
 const getdetails = async (page) => {
-  console.log(page);
-  let values = await ShopOrderClone.aggregate([
-    {
-      $lookup: {
-        from: 'b2bshopclones',
-        localField: 'Uid', //Uid
-        foreignField: 'Uid', //Uid
-        as: 'userData',
-      },
-    },
-    {
-      $unwind: '$userData',
-    },
-    {
-      $lookup: {
-        from: 'productorderclones',
-        localField: '_id',
-        foreignField: 'orderId',
-        as: 'orderData',
-      },
-    },
+    console.log(page)
+    let values = await ShopOrderClone.aggregate([
+        {
+            $lookup: {
+                from: 'b2bshopclones',
+                localField: 'Uid', //Uid
+                foreignField: 'Uid', //Uid
+                as: 'userData',
+            },
+        },
+        {
+            $unwind: '$userData'
+        },
+        {
+            $lookup: {
+                from: 'productorderclones',
+                localField: '_id',
+                foreignField: 'orderId',
+                as: 'orderData',
+            }
+        },
 
-<<<<<<< HEAD
         {
             $project: {
                 shopId: 1,
@@ -47,43 +47,31 @@ const getdetails = async (page) => {
 
     return values;
 }
-=======
-    {
-      $project: {
-        shopId: 1,
-        shopType: '$userData.type',
-        shopName: '$userData.SName',
-        orderId: '$orderData.orderId',
-        totalItems: { $size: '$orderData' },
-      },
-    },
-    { $skip: 10 * page },
-    { $limit: 10 },
-  ]);
->>>>>>> 1e9f5d4571eb66a05a345ff2233782e84a52b17c
 
-  return values;
-};
 
 // GET PRODUCT DETAILS
 
+
 const getproductdetails = async (orderId) => {
-  let getproduct = await ShopOrderClone.findById(orderId);
-  return getproduct;
-};
+    let getproduct = await ShopOrderClone.findById(orderId)
+    return getproduct
+}
 
 // UPDATE PRODUCT DETAILS
 
 const updateProduct = async (id, updateBody) => {
-  let product = await ProductorderClone.findById(id);
-  if (!product) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'product not found');
-  }
-  product = await ProductorderClone.findByIdAndUpdate({ _id: id }, updateBody, { new: true });
-  return product;
-};
+    let product = await ProductorderClone.findById(id);
+    if (!product) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'product not found');
+    }
+    product = await ProductorderClone.findByIdAndUpdate({ _id: id }, updateBody, { new: true });
+    return product;
+}
+
+
 
 // UPDATE STATUS ACKNOWLEDGE
+
 
 // const updateAcknowledge = async (id) => {
 //     let acknowledge = await ShopOrderClone.findById(id);
@@ -100,7 +88,9 @@ const updateProduct = async (id, updateBody) => {
 //     return acknowledge;
 // };
 
+
 // UPDATE STATUS APPROVED
+
 
 // const updateApproved = async (id) => {
 //     let approved = await ShopOrderClone.findById(id);
@@ -117,7 +107,9 @@ const updateProduct = async (id, updateBody) => {
 //     return approved;
 // }
 
+
 //  UPDATE STATUS MODIFIED
+
 
 // const updateModified = async (id) => {
 //     let modified = await ShopOrderClone.findById(id);
@@ -134,23 +126,29 @@ const updateProduct = async (id, updateBody) => {
 //     return modified;
 // }
 
+
 //  UPDATE STATUS REJECTION
 
 const updateRejected = async (id, status) => {
-  let rejected = await ShopOrderClone.findById(id);
-  console.log(rejected);
-  if (!rejected) {
-    throw new ApiError(httpStatus.NOT_FOUND, ' not found');
-  }
-  rejected = await ShopOrderClone.findByIdAndUpdate({ _id: id }, { status: status }, { new: true });
-  console.log(rejected);
-  return rejected;
-};
+    let rejected = await ShopOrderClone.findById(id);
+    console.log(rejected);
+    if (!rejected) {
+        throw new ApiError(httpStatus.NOT_FOUND, ' not found')
+    }
+    rejected = await ShopOrderClone.findByIdAndUpdate(
+        { _id: id },
+        { status: status },
+        { new: true },
+    )
+    console.log(rejected);
+    return rejected;
+}
+
 
 //WARD LOADING EXECUTIVE
 
+
 const wardloadExecutive = async (page) => {
-<<<<<<< HEAD
 
     let data = await ShopOrderClone.aggregate([
 
@@ -197,72 +195,34 @@ const wardloadExecutive = async (page) => {
     return data;
 }
 
-=======
-  let data = await ShopOrderClone.aggregate([
-    {
-      $match: {
-        status: {
-          $in: ['Approved', 'Modified'],
-        },
-      },
-    },
-    {
-      $lookup: {
-        from: 'b2bshopclones',
-        localField: 'Uid', //Uid
-        foreignField: 'Uid', //Uid
-        as: 'userData',
-      },
-    },
-    {
-      $unwind: '$userData',
-    },
-    // {
-    //   $lookup: {
-    //     from: 'productorderclones',
-    //     localField: '_id',
-    //     foreignField: 'orderId',
-    //     as: 'orderData',
-    //   },
-    // },
-
-    // {
-    //   $project: {
-    //     shopId: 1,
-    //     status: 1,
-    //     shopType: '$userData.type',
-    //     shopName: '$userData.SName',
-    //     orderId: '$orderData.orderId',
-    //   },
-    // },
-    // { $skip: 10 * page },
-    // { $limit: 10 },
-  ]);
-  return data;
-};
->>>>>>> 1e9f5d4571eb66a05a345ff2233782e84a52b17c
 
 // TRACK STATUS FOR PRODUCT STATUS
 const updateBilled = async (id, productStatus) => {
-  let productOrderBilled = await ShopOrderClone.findById(id);
-  if (!productOrderBilled) {
-    throw new ApiError(httpStatus.NOT_FOUND, 'productOrderBilled not found');
-  }
-  productOrderBilled = await ShopOrderClone.findByIdAndUpdate({ _id: id }, { productStatus: productStatus }, { new: true });
-  console.log(productOrderBilled);
-  return productOrderBilled;
-};
+    let productOrderBilled = await ShopOrderClone.findById(id);
+    if (!productOrderBilled) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'productOrderBilled not found')
+    }
+    productOrderBilled = await ShopOrderClone.findByIdAndUpdate(
+        { _id: id },
+        { productStatus: productStatus },
+        { new: true }
+    )
+    console.log(productOrderBilled);
+    return productOrderBilled;
+}
 
 // AFTER PACKED BY WARD LOADING EXECUTE
 
+
 const wardloadExecutivePacked = async (page) => {
-  let data = await ShopOrderClone.aggregate([
-    {
-      $match: {
-        productStatus: {
-          $in: ['Packed'],
+    let data = await ShopOrderClone.aggregate([
+        {
+            $match: {
+                'productStatus':{
+                    $in: ['Packed']
+                }
+            }
         },
-<<<<<<< HEAD
         {
             $lookup: {
                 from: 'b2bshopclones',
@@ -324,79 +284,23 @@ const wardloadExecutivePacked = async (page) => {
 
     return data;
 }
-=======
-      },
-    },
-    {
-      $lookup: {
-        from: 'b2bshopclones',
-        localField: 'shopId',
-        foreignField: '_id',
-        as: 'shopData',
-      },
-    },
-    { $unwind: '$shopData' },
-    {
-      $lookup: {
-        from: 'streets',
-        localField: 'shopData.Strid',
-        foreignField: '_id',
-        as: 'streetsData',
-      },
-    },
-    { $unwind: '$streetsData' },
->>>>>>> 1e9f5d4571eb66a05a345ff2233782e84a52b17c
 
-    {
-      $lookup: {
-        from: 'productorderclones',
-        localField: '_id',
-        foreignField: 'orderId',
-        pipeline: [{ $group: { _id: null, Qty: { $sum: '$quantity' } } }],
-        as: 'orderData',
-      },
-    },
-    { $unwind: '$orderData' },
-    {
-      $lookup: {
-        from: 'productorderclones',
-        localField: '_id',
-        foreignField: 'orderId',
-        as: 'orderDatafortotal',
-      },
-    },
 
-    {
-      $project: {
-        type: '$shopData.type',
-        street: '$streetsData.street',
-        orderId: '$orderDatafortotal.orderId',
-        orderDate: '$orderDatafortotal.date',
-        orderTime: '$orderDatafortotal.time',
-        totalItems: { $size: '$orderDatafortotal' },
-        Qty: '$orderData.Qty',
-        // totalcount: '$orderData.totalItems'
-      },
-    },
-    { $skip: 10 * page },
-    { $limit: 10 },
-  ]);
 
-  return data;
-};
 
 module.exports = {
-  getdetails,
-  getproductdetails,
-  updateProduct,
-  // updateAcknowledge,
-  // updateApproved,
-  // updateModified,
-  updateRejected,
+    getdetails,
+    getproductdetails,
+    updateProduct,
+    // updateAcknowledge,
+    // updateApproved,
+    // updateModified,
+    updateRejected,
 
-  //WARD LOADING EXECUTIVE
-  wardloadExecutive,
-  updateBilled,
+    //WARD LOADING EXECUTIVE
+    wardloadExecutive,
+    updateBilled,
 
-  wardloadExecutivePacked,
-};
+
+    wardloadExecutivePacked,
+}
