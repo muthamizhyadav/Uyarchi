39a386c56f6b0808d981c741e1c7184e2dce5c57
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-09 11:58:28 +0530

    asdf

--- src/models/b2b.wardAdminGroupDetails.model.js
+++ src/models/b2b.wardAdminGroupDetails.model.js
@@ -4,42 +4,42 @@ const { toJSON, paginate } = require('./plugins');
 const moment = require('moment');
 
 const wardAdminGroupDetailsSchema = new mongoose.Schema({
-
-    _id: {
-        type: String,
-        default: v4,
-      },
-      productid: {
-        type: String,
-      },
-      quantity: {
-        type: Number,
-      },
-      priceperkg: {
-        type: Number,
-      },
-      OrderId: {
-        type: String,
-      },
-      userId: {
-        type: String,
-      },
-      date: {
-        type: String,
-      },
-      time: {
-        type: String,
-      },
-      active: {
-        type: Boolean,
-        default: true,
-      },
-      archive: {
-        type: Boolean,
-        default: false,
-        
-      },
-     
+  _id: {
+    type: String,
+    default: v4,
+  },
+  productid: {
+    type: String,
+  },
+  quantity: {
+    type: Number,
+  },
+  priceperkg: {
+    type: Number,
+  },
+  wardadmingroupsId: {
+    type: String,
+  },
+  userId: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
+  time: {
+    type: String,
+  },
+  active: {
+    type: Boolean,
+    default: true,
+  },
+  orderedTime: {
+    type: String,
+  },
+  archive: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 const wardAdminGroupDetailsModel = mongoose.model('wardAdminGroupDetails', wardAdminGroupDetailsSchema);
