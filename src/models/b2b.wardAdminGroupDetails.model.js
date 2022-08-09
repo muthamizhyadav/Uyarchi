5f7091051cd85a2516add7d0079a8c22de879774
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-09 12:18:03 +0530

    asdf

--- src/models/b2b.wardAdminGroupDetails.model.js
+++ src/models/b2b.wardAdminGroupDetails.model.js
@@ -4,44 +4,6 @@ const { toJSON, paginate } = require('./plugins');
 const moment = require('moment');
 
 const wardAdminGroupDetailsSchema = new mongoose.Schema({
-<<<<<<< HEAD
-
-    _id: {
-        type: String,
-        default: v4,
-    },
-    productid: {
-        type: String,
-    },
-    quantity: {
-        type: Number,
-    },
-    priceperkg: {
-        type: Number,
-    },
-    OrderId: {
-        type: String,
-    },
-    userId: {
-        type: String,
-    },
-    date: {
-        type: String,
-    },
-    time: {
-        type: String,
-    },
-    active: {
-        type: Boolean,
-        default: true,
-    },
-    archive: {
-        type: Boolean,
-        default: false,
-
-    },
-
-=======
   _id: {
     type: String,
     default: v4,
@@ -78,7 +40,6 @@ const wardAdminGroupDetailsSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
->>>>>>> 39a386c56f6b0808d981c741e1c7184e2dce5c57
 });
 
 const wardAdminGroupDetailsModel = mongoose.model('wardAdminGroupDetails', wardAdminGroupDetailsSchema);
