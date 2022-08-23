cf6c385f7bbbc3a3c3520f49303824af29adaafd
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-23 11:16:47 +0530

    asdf

--- src/services/b2b.callHistory.service.js
+++ src/services/b2b.callHistory.service.js
@@ -22,6 +22,7 @@ const createcallHistoryWithType = async (body, userId) => {
   let serverdate = moment().format('yyyy-MM-DD');
 
   const { callStatus, shopId, reason } = body;
+  console.log(body.currentdate)
   let sort;
   if (callStatus == 'reschedule') {
     sort = 2;
