3ef5678dbad82d90ad9f5d03060a671113617c9f
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-23 14:57:02 +0530

    sd

--- src/routes/v1/estimatedOrders.route.js
+++ src/routes/v1/estimatedOrders.route.js
@@ -9,4 +9,5 @@ router.route('/PH/:date/:page').get(estimateOrderController.getEstimatedByDatefo
 router.route('/product/Estimate/:id').get(estimateOrderController.getSingleProductEstimations);
 router.route('/:id').put(estimateOrderController.updateEstimatedOrders);
 router.route('/getEstimated/orders/:id').get(estimateOrderController.getEstimated_Orders_By_Id_And_date);
+router.route('/getLiveStocks/Info/:id').get(estimateOrderController.liveStockInfo);
 module.exports = router;
