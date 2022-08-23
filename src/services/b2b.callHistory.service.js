bc4b7d3fd951e06dcc8a2452ca01857b7be11f42
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-23 09:59:14 +0530

    asdfg

--- src/services/b2b.callHistory.service.js
+++ src/services/b2b.callHistory.service.js
@@ -705,7 +705,7 @@ const resethistory = async () => {
   let today = '';
   today = currentDate;
   await Shop.updateMany(
-    { $and: [{ sortdate: { $eq: yersterday }, callingStatus: { $ne: 'reschedule' } }] },
+    { $or: [{ sortdate: { $eq: yersterday }, callingStatus: { $ne: 'reschedule' } }] },
     { $set: { callingStatus: 'Pending', callingStatusSort: 0, sortdate: today } }
   );
   return { dayfresh: 'Reset Successfully' };
