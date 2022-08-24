4cef2ae548bc32b1b44c2a75a70c7c8e8199b711
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-24 10:25:32 +0530

    asdf

--- src/services/b2b.callHistory.service.js
+++ src/services/b2b.callHistory.service.js
@@ -46,24 +46,22 @@ const createcallHistoryWithType = async (body, userId) => {
       {
         callingStatus: callStatus,
         sorttime: time,
-        historydate: currentdate,
         callingStatusSort: sort,
         sortdate: dateSlice,
       },
       { new: true }
     );
-    await Shop.findByIdAndUpdate({ _id: shopId }, { historydate: currentdate }, { new: true });
   } else {
     if (callStatus != 'accept') {
       await Shop.findByIdAndUpdate(
         { _id: shopId },
-        { callingStatus: callStatus, sorttime: time, historydate: currentdate, callingStatusSort: sort },
+        { callingStatus: callStatus, sorttime: time, callingStatusSort: sort },
         { new: true }
       );
     } else {
       await Shop.findByIdAndUpdate(
         { _id: shopId },
-        { historydate: body.currentdate, callingStatusSort: sort, sortdate: date, historydate: currentdate },
+        {callingStatusSort: sort, sortdate: date},
         { new: true }
       );
     }
