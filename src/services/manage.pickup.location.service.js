f1e148ca780f436e4d9a5a5e5682ea036f081555
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-25 12:15:11 +0530

    sadf

--- src/services/manage.pickup.location.service.js
+++ src/services/manage.pickup.location.service.js
@@ -10,16 +10,16 @@ const createManagePickupLocation = async (body) => {
   // let latlan = await axios.get(
   //   `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${body.address}&key=AIzaSyDoYhbYhtl9HpilAZSy8F_JHmzvwVDoeHI`
   // );
-  // let servertime = moment().format('HHmm');
-  // let serverdate = moment().format('DD-MM-yyy');
+  let servertime = moment().format('HHmm');
+  let serverdate = moment().format('DD-MM-yyy');
   // if(latlan.data.results.length == 0){
   //   throw new ApiError(httpStatus.BAD_REQUEST, 'Address Not Valid');
   // }
   // let locations = latlan.data.results[0].geometry;
   // let latitude = locations.location.lat;
   // let langitude = locations.location.lng;
-  // let values = { ...body, ...{ latitude: latitude, langitude: langitude, date: serverdate, time: servertime } };
-  const createpickuplocations = await PickupLocation.create(body);
+  let values = { ...body, ...{ date: serverdate, time: servertime } };
+  const createpickuplocations = await PickupLocation.create(values);
   return createpickuplocations;
 };
 
