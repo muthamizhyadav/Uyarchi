94a172b4c93d4fc80b1f2e367fa5837fa1d03b3d
muthamizhyadav <muthamizhyadav@gmail.com>
2022-08-25 11:15:55 +0530

    asdf

--- src/services/manage.pickup.location.service.js
+++ src/services/manage.pickup.location.service.js
@@ -7,19 +7,19 @@ const { UserBindingPage } = require('twilio/lib/rest/ipMessaging/v2/service/user
 const { Users } = require('../models/B2Busers.model');
 
 const createManagePickupLocation = async (body) => {
-  let latlan = await axios.get(
-    `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${body.address}&key=AIzaSyDoYhbYhtl9HpilAZSy8F_JHmzvwVDoeHI`
-  );
-  let servertime = moment().format('HHmm');
-  let serverdate = moment().format('DD-MM-yyy');
-  if(latlan.data.results.length == 0){
-    throw new ApiError(httpStatus.BAD_REQUEST, 'Address Not Valid');
-  }
-  let locations = latlan.data.results[0].geometry;
-  let latitude = locations.location.lat;
-  let langitude = locations.location.lng;
-  let values = { ...body, ...{ latitude: latitude, langitude: langitude, date: serverdate, time: servertime } };
-  const createpickuplocations = await PickupLocation.create(values);
+  // let latlan = await axios.get(
+  //   `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${body.address}&key=AIzaSyDoYhbYhtl9HpilAZSy8F_JHmzvwVDoeHI`
+  // );
+  // let servertime = moment().format('HHmm');
+  // let serverdate = moment().format('DD-MM-yyy');
+  // if(latlan.data.results.length == 0){
+  //   throw new ApiError(httpStatus.BAD_REQUEST, 'Address Not Valid');
+  // }
+  // let locations = latlan.data.results[0].geometry;
+  // let latitude = locations.location.lat;
+  // let langitude = locations.location.lng;
+  // let values = { ...body, ...{ latitude: latitude, langitude: langitude, date: serverdate, time: servertime } };
+  const createpickuplocations = await PickupLocation.create(body);
   return createpickuplocations;
 };
 
