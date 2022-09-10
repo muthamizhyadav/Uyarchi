a19facd9eb31793c7b000ddcdb74f7eef3e2b672
muthamizhyadav <muthamizhyadav@gmail.com>
2022-09-10 10:32:18 +0530

    asdfg

--- src/services/b2b.wardAdmin.service.js
+++ src/services/b2b.wardAdmin.service.js
@@ -637,7 +637,7 @@ const getproductdetails = async (id) => {
         foreignField: 'orderId',
         pipeline: [
           {
-            $match: { $or: [{ finalQuantity: { $ne: null } }, { finalQuantity: { $gt: 0 } }] },
+            $match: { $or: [{ finalQuantity: { $ne: null } }, { finalQuantity: { $ne: 0 } }] },
           },
           {
             $lookup: {
