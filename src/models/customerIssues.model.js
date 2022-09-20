c4665a64351820e8e6b071ef4b94ba2ed9d197a0
muthamizhyadav <muthamizhyadav@gmail.com>
2022-09-20 16:00:48 +0530

    asdf

--- src/models/customerIssues.model.js
+++ src/models/customerIssues.model.js
@@ -42,6 +42,9 @@ const customerIssuesSchema = new mongoose.Schema({
   customerId: {
     type: String,
   },
+  orderId: {
+    type: String,
+  },
 });
 
 const CustomeIssues = mongoose.model('customerIssues', customerIssuesSchema);
