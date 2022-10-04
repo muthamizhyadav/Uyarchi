cd98228d32783b3d0ebe15a09277fcdcff65d72e
unknown <rohitkumar05999@gmail.com>
2022-10-04 10:15:49 +0530

    sdaf

--- src/services/wardAdminRole.service.js
+++ src/services/wardAdminRole.service.js
@@ -483,19 +483,18 @@ const createtemperaryAssigndata = async (body) => {
   let serverdate = moment().format('yyy-MM-DD');
   let time = moment().format('hh:mm a');
   body.arr.forEach(async (e) => {
-    let data = await SalesManShop.find({ salesManId: body.salesManId, shopId: e, status: 'Assign' });
+    let data = await SalesManShop.find({ salesManId: body.fromSalesManId, shopId: e, status: 'Assign' });
     if (data.length != 0) {
       data.forEach(async (f) => {
         await Shop.findByIdAndUpdate({ _id: f.shopId }, { salesManStatus: body.status }, { new: true });
         await SalesManShop.findByIdAndUpdate(
           { _id: f._id },
           {
-            salesManId: f.salesManId,
+            salesManId: f.fromSalesManId,
             shopId: f.shopId,
             status: body.status,
             reAssignDate: serverdate,
             reAssignTime: time,
-            fromSalesManId: body.fromSalesManId,
           },
           { new: true }
         );
