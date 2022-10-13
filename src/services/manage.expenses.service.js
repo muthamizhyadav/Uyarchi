c44453152e9c2b4806bc7880842a6cd11f3ef05c
muthamizhyadav <muthamizhyadav@gmail.com>
2022-10-13 16:16:08 +0530

    asdfasdfas

--- src/services/manage.expenses.service.js
+++ src/services/manage.expenses.service.js
@@ -2,6 +2,7 @@ const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
 const ManageExpenses = require('../models/manage.expenses.model');
 const moment = require('moment');
+
 const createManageExpenses = async (body) => {
   let values = { ...body, ...{ date: moment().format('YYYY-MM-DD'), created: moment() } };
   const manageExpense = await ManageExpenses.create(values);
