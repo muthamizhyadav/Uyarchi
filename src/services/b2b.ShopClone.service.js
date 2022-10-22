92fda08d0411be3a9f41a6b0b230d38cefaf65f2
muthamizhyadav <muthamizhyadav@gmail.com>
2022-10-22 14:43:18 +0530

    apagination issue cleared

--- src/services/b2b.ShopClone.service.js
+++ src/services/b2b.ShopClone.service.js
@@ -2404,7 +2404,7 @@ const getnotAssignSalesmanData = async (id, page, limit) => {
         address: 1,
         locality: '$streets.locality',
         _id: 1,
-        displaycount:1,
+        displaycount: 1,
       },
     },
     {
@@ -2605,7 +2605,20 @@ const GetShopsReviewsByShopType = async (id, page) => {
       },
     },
   ]);
-  return { shops: shops, total: total.length };
+  let totalcount = await Shop.aggregate([
+    {
+      $match: {
+        SType: '07299efb-1aa4-40e6-ad5f-a03ffecdc0a5',
+        SType: '66077e16-aa5f-401f-abcc-e1842d151b14',
+        SType: '2d2a9e39-34a1-4dde-9767-a37251854cc5',
+        SType: '57fdca99-9b2c-47aa-838b-eed600d3264a',
+        SType: 'c974636a-6324-4426-9440-d599353c9a18',
+        SType: '4372526e-266a-4474-a140-7e633015b15c',
+        SType: '602e637e-11e8-4901-b80f-db8a467afda2',
+      },
+    },
+  ]);
+  return { shops: shops, total: total.length, totalcount: totalcount.length };
 };
 
 const getShopReviewByShopid = async (id) => {
