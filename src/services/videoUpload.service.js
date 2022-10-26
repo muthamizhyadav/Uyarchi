ef18c72038b687724d7ea53793edd212bfa73e1a
muthamizhyadav <muthamizhyadav@gmail.com>
2022-10-26 12:14:33 +0530

    video upload

--- src/services/videoUpload.service.js
+++ src/services/videoUpload.service.js
@@ -3,8 +3,14 @@ const videoUploadService = require('../models/videoUpload.model');
 const moment = require('moment');
 const createVideoUpload = async (body) => {
   let values = { ...body, ...{ created: moment() } };
-  const video = await videoUploadService.create(values);
-  return video;
+  let shopfind = await videoUploadService.findOne({ shopId: body.shopId });
+  if (shopfind.length > 0) {
+    const update = await videoUploadService.findByIdAndUpdate({ _id: shopfind._id }, body, { new: true });
+    return update;
+  } else {
+    const video = await videoUploadService.create(values);
+    return video;
+  }
 };
 
 module.exports = {
