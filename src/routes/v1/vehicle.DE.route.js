91bf26f7a91fe471660cc6d7dbd58f57ab019c67
muthamizhyadav <muthamizhyadav@gmail.com>
2022-10-31 12:52:31 +0530

    stock

--- src/routes/v1/vehicle.DE.route.js
+++ src/routes/v1/vehicle.DE.route.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // create Vehicle Route && fetch Active Vehicles
 router
   .route('/')
-  .post(middleware.fields([{ name: 'RC_book_image' }, { name: 'vehicle_image' }]), vehicleController.createVehicle)
-  .get(vehicleController.getVehicle);
+  .post(middleware.fields([{ name: 'RC_book_image' }, { name: 'vehicle_image' }]), vehicleController.createVehicle);
+router.route('/:page').get(vehicleController.getVehicle);
 
 // fect deliveryExecutives && activeVehicles
 router.route('/De/vehicles').get(vehicleController.getVehicle_and_DE);
