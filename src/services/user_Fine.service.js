60b692fcb3d4feb232adf5bd02abda6f537c426c
bharathidev0303 <bharathiraja996574@gmail.com>
2022-11-10 17:23:12 +0530

    comments added

--- src/services/user_Fine.service.js
+++ src/services/user_Fine.service.js
@@ -1,15 +1,24 @@
 const httpStatus = require('http-status');
 const Userfine = require('../models/user_Fine.model');
 const ApiError = require('../utils/ApiError');
+const {
+    ShopOrderClone
+} = require('../models/shopOrder.model')
 const moment = require('moment');
 
 // create UserFine
 
 const createUserFine = async (body) => {
-    let values = { ...body, ...{ created: moment(), date: moment().format('YYYY-MM-DD'), time: moment().format('HH:mm:ss')
-}}
-const createValues = await Userfine.create(values)
-return createValues
+    let values = {
+        ...body, ...{
+            created: moment(), date: moment().format('YYYY-MM-DD'), time: moment().format('HH:mm:ss')
+        }
+    }
+    let orderId = body.orderId
+    console.log(orderId)
+    await ShopOrderClone.findByIdAndUpdate({ _id: orderId }, { creditApprovalStatus: "Approved" }, { new: true })
+    const createValues = await Userfine.create(values)
+    return createValues
 }
 
 
