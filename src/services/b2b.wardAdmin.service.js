94939b7ba92c6468e66bc44be19a9ac9de63f30c
Vidhya150620 <vidhyavidhu1506@gmail.com>
2022-11-17 14:03:42 +0530

    dfg

--- src/services/b2b.wardAdmin.service.js
+++ src/services/b2b.wardAdmin.service.js
@@ -110,7 +110,7 @@ const updateAcknowledgeSingle = async (id, updateBody, userId) => {
   return product;
 };
 
-const updateApprovedMultiSelect = async (body) => {
+const updateApprovedMultiSelect = async (body,userId) => {
   body.arr.forEach(async (e) => {
     await ShopOrderClone.findByIdAndUpdate({ _id: e }, { status: 'Approved', statusUpdate: moment(),approveCreated:  moment() }, { new: true });
   });
@@ -471,7 +471,7 @@ const updateProduct = async (id, updateBody,userId) => {
 
 //  UPDATE STATUS REJECTION
 
-const updateRejected = async (body) => {
+const updateRejected = async (body,userId) => {
   body.arr.forEach(async (e) => {
     await ShopOrderClone.findByIdAndUpdate({ _id: e }, { status: 'Acknowledged', statusUpdate: moment(), AcknowledgeCreated: moment()}, { new: true });
 
