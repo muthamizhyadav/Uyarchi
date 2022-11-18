e11655d9fe80bf204c1a0253c7554fb502287ca8
Bharathi Raja <bharathiraja996574@gmail.com>
2022-11-18 15:43:52 +0530

    zd

--- src/controllers/shopregister.controller.js
+++ src/controllers/shopregister.controller.js
@@ -37,7 +37,7 @@ const get_myDetails = catchAsync(async (req, res) => {
   res.status(httpStatus.CREATED).send(shop);
 });
 const get_myorder = catchAsync(async (req, res) => {
-  const shop = await registerShop.get_myorder(req);
+  const shop = await registerShop.get_myorder(req, req.query);
   res.status(httpStatus.CREATED).send(shop);
 });
 const get_mypayments = catchAsync(async (req, res) => {
