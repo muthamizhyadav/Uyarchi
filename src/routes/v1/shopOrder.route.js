855ad4d2c8db460d617c078e26038e12fa09c6e6
Bharathi Raja <bharathiraja996574@gmail.com>
2022-11-22 12:37:57 +0530

    zd

--- src/routes/v1/shopOrder.route.js
+++ src/routes/v1/shopOrder.route.js
@@ -66,5 +66,6 @@ router.route('/ward/Admin/order/particular/order/:id').get(shopOrderController.O
 router.route('/ward/Admin/order/particular/order/details/:id').get(shopOrderController.details_Of_Payment_by_Id);
 router.route('/payment/history/byOrder/:id').get(shopOrderController.getPaymenthistory);
 router.route('/getallmanage/issues').get(shopOrderController.getallmanageIssus);
+router.route('/getmanage/issues/byID').get(shopOrderController.getmanageIssus_byID);
 
 module.exports = router;
