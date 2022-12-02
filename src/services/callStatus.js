4c57cfaecf88496d5244d7878e98b4977e75ae01
muthamizhyadav <muthamizhyadav@gmail.com>
2022-12-02 10:53:32 +0530

    payable flow

--- src/services/callStatus.js
+++ src/services/callStatus.js
@@ -400,6 +400,154 @@ const suddenOrdersDisplay = async (productId) => {
   return values;
 };
 
+const getReportWithSupplierId = async (id, page, search, date) => {
+  console.log(search);
+  let dateM = { active: true };
+  let searchMatch = { active: true };
+  if (search !== 'null') {
+    searchMatch = { primaryContactName: { $regex: search, $options: 'i' } };
+  } else {
+    searchMatch;
+  }
+  if (date !== 'null') {
+    dateM = { date: { $eq: date } };
+  } else {
+    dateM;
+  }
+  let values = await CallStatus.aggregate([
+    {
+      $match: {
+        $and: [
+          { supplierid: { $eq: id } },
+          { StockReceived: { $eq: 'Pending' } },
+          { confirmcallstatus: { $eq: 'Accepted' } },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'products',
+        localField: 'productid',
+        foreignField: '_id',
+        as: 'ProductData',
+      },
+    },
+    {
+      $unwind: '$ProductData',
+    },
+    {
+      $lookup: {
+        from: 'suppliers',
+        localField: 'supplierid',
+        foreignField: '_id',
+        pipeline: [
+          {
+            $match: {
+              $and: [searchMatch],
+            },
+          },
+        ],
+        as: 'supplierData',
+      },
+    },
+    {
+      $unwind: '$supplierData',
+    },
+    {
+      $project: {
+        _id: 1,
+        active: 1,
+        StockReceived: 1,
+        qtyOffered: 1,
+        strechedUpto: 1,
+        price: 1,
+        status: 1,
+        requestAdvancePayment: 1,
+        callstatus: 1,
+        callDetail: 1,
+        productid: 1,
+        supplierid: 1,
+        date: 1,
+        time: 1,
+        phApproved: 1,
+        phStatus: 1,
+        phreason: 1,
+        confirmOrder: 1,
+        orderType: 1,
+        confirmcallDetail: 1,
+        confirmcallstatus: 1,
+        confirmprice: 1,
+        exp_date: 1,
+        ExpectedDate: { $ifNull: ['$Expdate', 'null'] },
+        supplierContact: '$supplierData.primaryContactNumber',
+        supplierName: '$supplierData.primaryContactName',
+        productTitle: '$ProductData.productTitle',
+      },
+    },
+    {
+      $match: {
+        $and: [dateM],
+      },
+    },
+    { $skip: 10 * page },
+    { $limit: 10 },
+  ]);
+  let total = await CallStatus.aggregate([
+    {
+      $match: {
+        $and: [
+          { supplierid: { $eq: id } },
+          { StockReceived: { $eq: 'Pending' } },
+          { confirmcallstatus: { $eq: 'Accepted' } },
+          // {SuddenStatus:{$eq:'Approve'}},
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'products',
+        localField: 'productid',
+        foreignField: '_id',
+        as: 'ProductData',
+      },
+    },
+    {
+      $unwind: '$ProductData',
+    },
+    {
+      $lookup: {
+        from: 'suppliers',
+        localField: 'supplierid',
+        foreignField: '_id',
+        as: 'supplierData',
+      },
+    },
+    {
+      $unwind: '$supplierData',
+    },
+  ]);
+  let totalss = await CallStatus.aggregate([
+    {
+      $match: {
+        $and: [
+          { supplierid: { $eq: id } },
+          { StockReceived: { $eq: 'Pending' } },
+          { confirmcallstatus: { $eq: 'Accepted' } },
+          { SuddenStatus: { $eq: 'Approve' } },
+        ],
+      },
+    },
+    {
+      $group: {
+        _id: { $dateToString: { format: '%Y-%m-%d', date: '$created' } },
+        count: { $sum: 1 },
+      },
+    },
+  ]);
+  let getSupplier = await Supplier.findById(id);
+  return { values: values, total: total.length, supplier: getSupplier, totalss: totalss };
+};
+
 module.exports = {
   createCallStatus,
   getCallStatusById,
@@ -410,4 +558,5 @@ module.exports = {
   finishOrder,
   getCallstatusForSuddenOrders,
   suddenOrdersDisplay,
+  getReportWithSupplierId,
 };
