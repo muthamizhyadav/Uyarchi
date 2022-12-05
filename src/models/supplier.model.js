b4619a783e6c855d73686b17edbb46b34592a150
unknown <rohitkumar05999@gmail.com>
2022-12-05 12:18:49 +0530

    erfer

--- src/models/supplier.model.js
+++ src/models/supplier.model.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const { v4 } = require('uuid');
 const { toJSON, paginate } = require('./plugins');
+const bcrypt = require('bcryptjs');
 
 const supplierSchema = new mongoose.Schema({
   _id: {
@@ -59,6 +60,9 @@ const supplierSchema = new mongoose.Schema({
   approvedStatus:{
     type:String,
   },
+  password:{
+    type:String,
+  },
   active: {
     type: Boolean,
     default: true,
@@ -71,6 +75,18 @@ const supplierSchema = new mongoose.Schema({
 
 supplierSchema.plugin(toJSON);
 supplierSchema.plugin(paginate);
+supplierSchema.methods.isPasswordMatch = async function (password) {
+  const user = this;
+  return bcrypt.compare(password, user.password);
+};
+
+supplierSchema.pre('save', async function (next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+  next();
+});
 const Supplier = mongoose.model('Supplier', supplierSchema);
 
 module.exports = Supplier;
