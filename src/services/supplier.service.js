a219b51f689f281baa7afa03f5fe38d92dc92bc7
muthamizhyadav <muthamizhyadav@gmail.com>
2022-12-05 17:30:42 +0530

    sadf

--- src/services/supplier.service.js
+++ src/services/supplier.service.js
@@ -8,16 +8,16 @@ const B2bBillStatus = require('../models/b2bbillStatus.model');
 const moment = require('moment');
 
 const createSupplier = async (supplierBody) => {
-  const check = await Supplier.findOne({ primaryContactNumber: supplierBody.primaryContactNumber })
+  const check = await Supplier.findOne({ primaryContactNumber: supplierBody.primaryContactNumber });
   // console.log(check)
   if (check) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Already Register this Number');
   }
-  if(supplierBody.createdByStatus == "By Supplier"){
-  if(supplierBody.password != supplierBody.confirmpassword){
-    throw new ApiError(httpStatus.NOT_FOUND, 'Confirm password not match');
+  if (supplierBody.createdByStatus == 'By Supplier') {
+    if (supplierBody.password != supplierBody.confirmpassword) {
+      throw new ApiError(httpStatus.NOT_FOUND, 'Confirm password not match');
+    }
   }
-}
   return Supplier.create(supplierBody);
 };
 
@@ -35,7 +35,6 @@ const UsersLogin = async (userBody) => {
   }
   return userName;
 };
-
 
 const getAllSupplier = async () => {
   return Supplier.find({ active: true });
@@ -465,19 +464,24 @@ const getSupplierWith_Advanced = async () => {
         tradeName: 1,
         raised: { $ifNull: ['$raised.raised_Amt', 0] },
         unbilled: { $ifNull: ['$supplierunbilled.un_Billed_amt', 0] },
+        RaisedAmount: {
+          $ifNull: [{ $ifNull: ['$raised.raised_Amt', 0] }, { $ifNull: ['$supplierunbilled.un_Billed_amt', 0] }, 0],
+        },
       },
     },
-    {
-      $project: {
-        _id: 1,
-        secondaryContactName: 1,
-        primaryContactName: 1,
-        primaryContactNumber: 1,
-        primaryContactName: 1,
-        tradeName: 1,
-        RaisedAmount: { $subtract: ['$raised', '$unbilled'] },
-      },
-    },
+    // {
+    //   $project: {
+    //     _id: 1,
+    //     secondaryContactName: 1,
+    //     primaryContactName: 1,
+    //     primaryContactNumber: 1,
+    //     primaryContactName: 1,
+    //     tradeName: 1,
+    //     raised: 1,
+    //     unbilled: 1,
+    //     RaisedAmount: { $subtract: ['$unbilled', '$raised'] },
+    //   },
+    // },
   ]);
   return values;
 };
