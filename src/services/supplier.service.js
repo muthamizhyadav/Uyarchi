55d25ed99f4562a288c3d070da48fbe27a62372d
muthamizhyadav <muthamizhyadav@gmail.com>
2022-12-16 12:26:40 +0530

    sadf

--- src/services/supplier.service.js
+++ src/services/supplier.service.js
@@ -716,11 +716,14 @@ const getSupplierWith_Advanced = async () => {
 const createSuppliers = async (body, userId) => {
   let values = { ...body, ...{ created: moment(), createdBy: userId } };
   const validate = await Supplier.find({ primaryContactNumber: body.primaryContactNumber });
-  if (validate.lenth !== 0) {
+  console.log(validate.length);
+  let len = validate.length
+  if (len > 0) {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Mobile Number Already Registered');
+  } else {
+    const create = await Supplier.create(values);
+    return create;
   }
-  const create = await Supplier.create(values);
-  return create;
 };
 
 const getSupplierthird = async (key, page) => {
