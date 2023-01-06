ca66e7de7e1c2d4f2e0adb70eb97b7ffaaed66e7
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-01-06 13:20:28 +0530

    s

--- src/routes/v1/ecomplan.route.js
+++ src/routes/v1/ecomplan.route.js
@@ -6,6 +6,6 @@ router.route('/create/plan').post(Ecomcontroller.create_Plans)
 router.route('/get/all/plan').get(Ecomcontroller.get_all_Plans)
 router.route('/get/one/plan').get(Ecomcontroller.get_one_Plans)
 router.route('/update/one/plan').put(Ecomcontroller.update_one_Plans)
-router.route('/delete/one/plan').get(Ecomcontroller.delete_one_Plans)
+router.route('/delete/one/plan').delete(Ecomcontroller.delete_one_Plans)
 
 module.exports = router;
