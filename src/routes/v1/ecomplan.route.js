241c423b4e4f374b05560a214c43e7470ace76c6
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-01-09 16:26:21 +0530

    s

--- src/routes/v1/ecomplan.route.js
+++ src/routes/v1/ecomplan.route.js
@@ -2,10 +2,20 @@ const express = require('express');
 // const customerController = require('../../controllers/customer.controller');
 const Ecomcontroller = require('../../controllers/ecomplan.controller');
 const router = express.Router();
+const supplierAuth = require('../../controllers/supplier.authorizations');
+
+// plan APIS
 router.route('/create/plan').post(Ecomcontroller.create_Plans)
 router.route('/get/all/plan').get(Ecomcontroller.get_all_Plans)
 router.route('/get/one/plan').get(Ecomcontroller.get_one_Plans)
 router.route('/update/one/plan').put(Ecomcontroller.update_one_Plans)
 router.route('/delete/one/plan').delete(Ecomcontroller.delete_one_Plans)
+
+// post APIS
+router.route('/create/post').post(supplierAuth,Ecomcontroller.create_post)
+router.route('/get/all/post').get(supplierAuth,Ecomcontroller.get_all_post)
+router.route('/get/one/post').get(supplierAuth,Ecomcontroller.get_one_post)
+router.route('/update/one/post').put(supplierAuth,Ecomcontroller.update_one_post)
+router.route('/delete/one/post').delete(supplierAuth,Ecomcontroller.delete_one_post)
 
 module.exports = router;
--- src/services/ecomplan.service.js
+++ src/services/ecomplan.service.js
@@ -1,5 +1,5 @@
 const httpStatus = require('http-status');
-const { Streamplan } = require('../models/ecomplan.model');
+const { Streamplan, StreamPost } = require('../models/ecomplan.model');
 const ApiError = require('../utils/ApiError');
 
 const Date = require('./Date.serive')
@@ -25,21 +25,65 @@ const get_all_Plans = async (req) => {
 
 
 const get_one_Plans = async (req) => {
-    let page = req.query.page == '' || req.query.page == null || req.query.page == null ? 0 : req.query.page;
     const value = await Streamplan.findById(req.query.id);
     return value;
 };
 
 
 const update_one_Plans = async (req) => {
-    const value = await Streamplan.findByIdAndUpdate({_id: req.query.id},req.body,{new:true})
+    const value = await Streamplan.findByIdAndUpdate({ _id: req.query.id }, req.body, { new: true })
     return value;
 };
 
 
 const delete_one_Plans = async (req) => {
-   await Streamplan.findByIdAndDelete({_id: req.query.id});
-    return {message:"deleted"};
+    await Streamplan.findByIdAndDelete({ _id: req.query.id });
+    return { message: "deleted" };
+};
+
+const create_post = async (req) => {
+    const value = await StreamPost.create(req.body)
+    await Date.create_date(value)
+    value.suppierid = req.userId;
+    value.save();
+    return value;
+};
+
+const get_all_Post = async (req) => {
+    let page = req.query.page == '' || req.query.page == null || req.query.page == null ? 0 : req.query.page;
+    const value = await StreamPost.aggregate([
+        { $match: { $and: [{ suppierId: { $eq: req.userId } }] } },
+        { $sort: { DateIso: -1 } },
+        { $skip: 10 * page },
+        { $limit: 10 },
+    ])
+    return value;
+};
+
+
+
+const get_one_Post = async (req) => {
+    const value = await StreamPost.findById(req.query.id);
+    if (value.suppierId !=req.userId) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Not Found');
+      }
+    return value;
+};
+
+
+const update_one_Post = async (req) => {
+    const value = await StreamPost.findByIdAndUpdate({ _id: req.query.id, suppierId: req.userId }, req.body, { new: true })
+    if (!value) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Not Found');
+      }
+    return value;
+};
+const delete_one_Post = async (req) => {f
+    const value = await StreamPost.findByIdAndDelete({ _id: req.query.id, suppierId: req.userId });
+    if (!value) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'Not Found');
+      }
+    return { message: "deleted" };
 };
 
 module.exports = {
@@ -47,5 +91,10 @@ module.exports = {
     get_all_Plans,
     get_one_Plans,
     update_one_Plans,
-    delete_one_Plans
+    delete_one_Plans,
+    create_post,
+    get_all_Post,
+    get_one_Post,
+    update_one_Post,
+    delete_one_Post
 };
