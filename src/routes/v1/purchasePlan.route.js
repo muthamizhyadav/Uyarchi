e2aed865d8c2296e258d792e18c6cee2764c3f7b
bharathi raja <bharathiraja@bharathis-MacBook-Pro.local>
2023-01-09 10:59:54 +0530

    s

--- src/routes/v1/purchasePlan.route.js
+++ src/routes/v1/purchasePlan.route.js
@@ -5,4 +5,7 @@ const supplierAuth = require('../../controllers/supplier.authorizations');
 const router = express.Router();
 
 router.route('/purchase/suceess').post(supplierAuth,purchasePlan.create_purchase_plan);
+router.route('/getpayment/details/one').get(supplierAuth,purchasePlan.get_order_details);
+router.route('/getpayment/details/all').get(supplierAuth,purchasePlan.get_all_my_orders);
+
 module.exports = router;
--- src/services/purchasePlan.service.js
+++ src/services/purchasePlan.service.js
@@ -15,23 +15,58 @@ const create_purchase_plan = async (req) => {
         console.log(payment)
         let collectedAmount = payment.amount / 100
         let collectedstatus = payment.status;
-        let plan=await Streamplan.findById(req.body.plan);
-        if (collectedstatus == 'captured' && collectedAmount ==plan.salesPrice ) {
-           let con= await purchasePlan.create({...{planId:req.body.plan,suppierId:req.userId,paidAmount:collectedAmount,paymentStatus:collectedstatus,order_id:payment.order_id},...req.body.PaymentDatails});
-           await Date.create_date(con)
-          return con;
+        let plan = await Streamplan.findById(req.body.plan);
+        if (collectedstatus == 'captured' && collectedAmount == plan.salesPrice) {
+            let con = await purchasePlan.create({ ...{ planId: req.body.plan, suppierId: req.userId, paidAmount: collectedAmount, paymentStatus: collectedstatus, order_id: payment.order_id }, ...req.body.PaymentDatails });
+            await Date.create_date(con)
+            return con;
         }
-        else{
-            return {error:"Amount Not Match"}
+        else {
+            return { error: "Amount Not Match" }
         }
     }
-    else{
-        return {error:"order not found"}
+    else {
+        return { error: "order not found" }
     }
+
 
 }
 
+const get_order_details = async (req) => {
+    let order = await purchasePlan.findById(req.query.id);
+    if (!plan || plan.suppierId != req.userId) {
+        throw new ApiError(httpStatus.NOT_FOUND, 'User Not Found');
+    }
+    let plan = await Streamplan.findById(order.planId);
+    let payment = await paymentgatway.verifyRazorpay_Amount({ razorpay_order_id: order.razorpay_order_id, razorpay_payment_id: order.razorpay_payment_id, razorpay_signature: order.razorpay_signature });
+
+    return { payment, plan, order }
+}
+
+const get_all_my_orders = async (req) => {
+    let plan = await purchasePlan.aggregate([
+        { $match: { suppierId: req.userId } },
+        {
+            $lookup: {
+                from: 'streamplans',
+                localField: 'planId',
+                foreignField: '_id',
+                as: 'streamplans',
+            },
+        },
+        {
+            $unwind: {
+                path: '$streamplans',
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+    ])
+
+    return plan;
+}
 
 module.exports = {
-    create_purchase_plan
+    create_purchase_plan,
+    get_order_details,
+    get_all_my_orders
 }
